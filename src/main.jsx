265fa37fb488faa449396df26d43205093859b59
Jackiiez <u2440308@rts-365.dk>
2025-05-26 09:20:46 +0200

    .

--- src/main.jsx
+++ src/main.jsx
@@ -9,17 +9,11 @@ import SeeMore from './pages/seemore.jsx'
 import CheckoutForm from "./pages/checkout.jsx";
 import Seats from './pages/seats.jsx'
 import Checkout from './pages/checkout.jsx'
-// import Details from "./details.jsx";
-// import Search from "./search.jsx";
-// import Searchpage from "./searchpage.jsx";
-// import Cast from "./cast-details.jsx";
-// import Genre from "./genre-details.jsx";
-// import GenreListFetch from "./genre-list.jsx";
 
 
 
 
-//router hvor vi putter komponenter ind og laver flere sider
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -56,40 +50,8 @@ const router = createBrowserRouter([
 
 
 
-  // {
-  //   //vi definere vores details side path 
-  //   path: "/details/:movieId",
-  //   element: <Details />
-  // },
-  // {
-
-  //   path: "/search",
-  //   element: <Search />
-  // }
-  // ,
-  // {
-
-  //   path: "/searchpage",
-  //   element: <Searchpage />
-  // },
-
-
-  // {
-
-  //   path: "/cast-details/:media_type/:castId",
-  //   element: <Cast />
-  // }, {
-
-  //   path: "/genre-details/:genreId",
-  //   element: <Genre />
-  // },
-  // {
-
-  //   path: "/genre-list/",
-  //   element: <GenreListFetch />
-  // }
 ])
-//vi laver et state som vi ka gemme i vores context
+
 const MyProvider = ({ children }) => {
   const [color, setColor] = useState("light");
   return <GloabelContext.Provider value={{ color, setColor }}>{children}</GloabelContext.Provider>;
