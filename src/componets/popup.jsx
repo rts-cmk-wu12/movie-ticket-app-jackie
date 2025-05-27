bae67f8db26bf46a3f78f612544a8542df3aaffa
Davidski7 <U2486889@rts-365.dk>
2025-05-27 09:32:19 +0200

    Jeg er igang med popup

---
+++ src/componets/popup.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import '../style/seats.scss';
+import { Link } from "react-router";
+
+function PopUp() {
+
+
+
+    return (
+        <h1></h1>
+    );
+}
+
+export default PopUp;
+
+
