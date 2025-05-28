dd0ffec99b44ff56e73f99c4901b9aa56c5f2da2
Davidski7 <U2486889@rts-365.dk>
2025-05-28 08:46:27 +0200

    hER

--- src/pages/ticket.jsx
+++ src/pages/ticket.jsx
@@ -1,22 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import '../style/ticket.scss';
 const Ticket = () => {
-        const [movies, setMovies] = useState([]);
-    
-        useEffect(() => {
-            fetch('https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1', {
-                method: 'GET',
-                headers: {
-                    accept: 'application/json',
-                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYWFlMTFlNTllZGRkZmJkM2NlZGIwYTEzYjI3YjRlOCIsIm5iZiI6MTczNjc1NTEyMC4xNzIsInN1YiI6IjY3ODRjN2IwMTQzMWUwNTkxYWJiYTlkNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bCFVQg_MtZlxXXSHpQYyN1rhaBF1FE7Gpw6C8LMaJ5g'
-                }
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1', {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYWFlMTFlNTllZGRkZmJkM2NlZGIwYTEzYjI3YjRlOCIsIm5iZiI6MTczNjc1NTEyMC4xNzIsInN1YiI6IjY3ODRjN2IwMTQzMWUwNTkxYWJiYTlkNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bCFVQg_MtZlxXXSHpQYyN1rhaBF1FE7Gpw6C8LMaJ5g'
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                setMovies(data.results.slice(0, 10));
             })
-                .then(response => response.json())
-                .then(data => {
-                    setMovies(data.results.slice(0, 10));
-                })
-                .catch(error => console.error('Fejl ved hentning:', error));
-        }, []);
+            .catch(error => console.error('Fejl ved hentning:', error));
+    }, []);
     const [bookingDetails, setBookingDetails] = useState(null);
 
     useEffect(() => {
@@ -26,7 +26,7 @@ const Ticket = () => {
 
     return (
         <>
-        {/* <div className="movies-grid">
+            {/* <div className="movies-grid">
                 {movies.map(movie => (
                     <div key={movie.id} className="movie-card">
                       
@@ -39,20 +39,20 @@ const Ticket = () => {
                     </div>
                 ))}
             </div> */}
-        <div className='ticket'>
-            <h2>Booking Information</h2>
-            {bookingDetails ? (
-                <>
-                    <p><strong>Cinema:</strong> {bookingDetails.cinema}</p>
-                    <p><strong>Date:</strong> {bookingDetails.date}</p>
-                    <p><strong>Time:</strong> {bookingDetails.time}</p>
-                    <p><strong>Seats:</strong> {bookingDetails.seats.join(', ')}</p>
-                    <p><strong>Total Price:</strong> ${bookingDetails.totalPrice}</p>
-                </>
-            ) : (
-                <div>No booking information found.</div>
-            )}
-        </div></>
+            <div className='ticket'>
+                <h2>Booking Information</h2>
+                {bookingDetails ? (
+                    <>
+                        <p><strong>Cinema:</strong> {bookingDetails.cinema}</p>
+                        <p><strong>Date:</strong> {bookingDetails.date}</p>
+                        <p><strong>Time:</strong> {bookingDetails.time}</p>
+                        <p><strong>Seats:</strong> {bookingDetails.seats.join(', ')}</p>
+                        <p><strong>Total Price:</strong> ${bookingDetails.totalPrice}</p>
+                    </>
+                ) : (
+                    <div>No booking information found.</div>
+                )}
+            </div></>
     );
 };
 
