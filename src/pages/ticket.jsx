9900180da652f43a96afc393f5a5bd1e14fe2911
Davidski7 <U2486889@rts-365.dk>
2025-05-28 12:56:03 +0200

    Jeg har lavet pop nummer2

--- src/pages/ticket.jsx
+++ src/pages/ticket.jsx
@@ -4,29 +4,23 @@ import TicketTop from '../componets/tickettop';
 import { Link } from 'react-router';
 import "../style/detailsmovie.scss";
 import PopUpTicket from '../componets/popupticket.jsx';
+
 const Ticket = () => {
     const [bookingDetails, setBookingDetails] = useState(null);
     const [showPopup, setShowPopup] = useState(false);
-
     const [movieTitle, setMovieTitle] = useState("");
-
 
     useEffect(() => {
         const bookingInfo = localStorage.getItem("bookingInfo");
         setBookingDetails(bookingInfo ? JSON.parse(bookingInfo) : null);
 
-
-
         const storedTitle = localStorage.getItem(`movie_id`);
         setMovieTitle(storedTitle);
-
     }, []);
-
 
     const handleShowPopup = () => {
         setShowPopup(true);
     };
-
 
     const handleClosePopup = () => {
         setShowPopup(false);
@@ -34,35 +28,38 @@ const Ticket = () => {
 
     return (
         <>
+            <TicketTop />
+            <div className='ticket'>
+                {bookingDetails ? (
+                    <>
+                        <div>
+                            <p><strong>Movie Title:</strong> {movieTitle}</p>
+                            <p>e-ticket</p>
+                        </div>
+                        <p><strong>Date:</strong> {bookingDetails.date}</p>
+                        <p><strong>Seats:</strong> {bookingDetails.seats.join(', ')}</p>
+                        <p><strong>Cinema:</strong> {bookingDetails.cinema}</p>
+                        <p><strong>Time:</strong> {bookingDetails.time}</p>
+                        <p><strong>Payment: </strong>Successful</p>
+                        <p><strong>Order: </strong>01</p>
+                    </>
+                ) : (
+                    <div>No booking information found.</div>
+                )}
+            </div>
 
-            {bookingDetails ? (
-                <> <div>
-                    <p><strong>Film</strong> {movieTitle}</p>
-                    <p>e-ticket</p></div>
-                    <p><strong>Date:</strong> {bookingDetails.date}</p>
-                    <p><strong>Seats:</strong> {bookingDetails.seats.join(', ')}</p>
-                    <p><strong>Cinema:</strong> {bookingDetails.cinema}</p>
-                    <p><strong>Time:</strong> {bookingDetails.time}</p>
-                    <p><strong>Payment: </strong>Successful</p>
-                    <p><strong>Order: </strong>01</p>
+            <div className="details-container">
+                <div className="details-info">
+                    <button className="book-button" onClick={handleShowPopup}>
+                        Download E-Ticket
+                    </button>
+                </div>
+            </div>
 
-                </>
-            ) : (
-                <div>No booking information found.</div>
-            )}
 
-            <div className="details-container"
-            >  <div className="details-info">
-                    <Link to="/">
-                        <button className="book-button">Download E-Ticket</button>
-                    </Link></div></div>
+            <PopUpTicket show={showPopup} />
         </>
     );
 };
 
 export default Ticket;
-
-
-
-
-
